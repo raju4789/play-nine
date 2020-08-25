a742e06796e1e65a5017f7a8cd893314615764fc
RajuTinku <narasimha4789@gmail.com>
2020-08-25 21:57:14 +0530

    added score func

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,10 +8,14 @@ import PlayAgain from './PlayAgain';
 
 // custom hook
 const useGameState = () => {
+    console.log(sessionStorage.getItem('highScore'))
     const [stars, setStars] = useState(UTILS.random(1, 9));
     const [availableNums, setAvailableNums] = useState(UTILS.range(1, 9));
     const [candidateNums, setCandidateNums] = useState([]);
     const [secondsLeft, setSecondsLeft] = useState(10);
+    const [highScore, setHighScore] = useState(localStorage.getItem('highScore') || 0);
+    const [score, setScore] = useState(0);
+    const [gameOver, setGameOver] = useState(false);
 
     useEffect(() => {
         if (secondsLeft > 0 && availableNums.length > 0) {
@@ -39,22 +43,32 @@ const useGameState = () => {
         setAvailableNums(UTILS.range(1, 9));
         setCandidateNums([]);
         setSecondsLeft(10);
-    };
-
-    return { stars, availableNums, candidateNums, secondsLeft, setGameState, resetGame };
-};
-
-const Game = (props) => {
-    const { stars, availableNums, candidateNums, secondsLeft, setGameState, resetGame } = useGameState();
-
-    const areCandidatesWrong = () => {
-        return UTILS.sum(candidateNums) > stars;
+        setScore(0);
+        setGameOver(false);
     };
 
     const gameStatus = (availableNums.length === 0) ? 'won' :
         (secondsLeft === 0) ? 'lost' : 'active';
 
+    if (!gameOver && (gameStatus === 'won' || gameStatus === 'lost')) {
+        setGameOver(true);
+        setScore(secondsLeft);
+        if (score > highScore) {
+            sessionStorage.setItem('highScore', score);
+            console.log(localStorage.getItem('highScore'))
+            setHighScore(score);
+        }
+    }
 
+    return { stars, availableNums, candidateNums, secondsLeft, gameStatus, score, highScore, setGameState, resetGame };
+};
+
+const Game = () => {
+    const { stars, availableNums, candidateNums, secondsLeft, gameStatus, score, highScore, setGameState, resetGame } = useGameState();
+
+    const areCandidatesWrong = () => {
+        return UTILS.sum(candidateNums) > stars;
+    };
 
     const numberStatus = (number) => {
 
@@ -82,9 +96,17 @@ const Game = (props) => {
 
     return (
         <div className="game">
+            <div className="body">
+                <div className="left">
+                    High Score :{highScore}
+                </div>
+                <div className="right">
+                    Score :{score}
+                </div>
+            </div>
             <div className="help">
                 Pick 1 or more numbers that sum to the number of stars
-            </div>
+        </div>
             <div className="body">
                 <div className="left">
                     {
@@ -105,7 +127,7 @@ const Game = (props) => {
                 </div>
             </div>
             <div className="timer">Time Remaining: {secondsLeft}</div>
-        </div>
+        </div >
     );
 };
 
