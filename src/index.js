85dae0a8ed7f8447f6642ec9c0ef418042d27116
Raju MLN <raju4789@Rajus-MacBook-Pro.local>
2025-11-11 09:13:01 +0700

    updated dependeencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 serviceWorker.unregister();
